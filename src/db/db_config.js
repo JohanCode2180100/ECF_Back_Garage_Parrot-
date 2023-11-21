9487eadd19b874b9d2f81cdeba0aea9ad8dd2814
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-21 14:43:31 +0100

    add variable db environnement for production

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql2");
 
-const dataConfigDB = {
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || "",
-  database: process.env.DB_NAME || "garageParrot",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-};
 const db = mysql.createConnection({
-  host: dataConfigDB.host,
-  port: dataConfigDB.port,
-  database: dataConfigDB.database,
-  user: dataConfigDB.user,
-  password: dataConfigDB.password,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 db.connect(function (err) {
