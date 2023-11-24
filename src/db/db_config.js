7809f2fccdb3240400d6b9ef90b50183adb466da
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-24 11:25:56 +0100

    add db

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,12 +1,24 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-  "mysql://f8u0cskxvdi841jj:q8zl21d4xuys8k3e@wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g0tvm76q649v2pp0"
-);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
 
 db.connect(function (err) {
   if (err) throw err;
   console.log("Connecté à la base de données MySQL!");
+});
+
+db.end(function (err) {
+  if (err) {
+    console.error("Erreur lors de la fermeture de la connexion :", err);
+  } else {
+    console.log("Connexion à la base de données fermée");
+  }
 });
 
 module.exports = db;
