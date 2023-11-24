4fa33c6b05ad5943887a77a2360526f7e5e44171
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-24 11:14:30 +0100

    changed conenction db

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.dbconnect);
+const db = mysql.createConnection({
+  host: process.env.host,
+  database: process.env.Database,
+  username: process.env.Username,
+  port: 3306,
+  password: process.env.Password,
+});
 
 db.connect(function (err) {
   if (err) throw err;
