fb830e3a61035a4f9fe0e436906c69e3c9076f4d
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-24 11:16:52 +0100

    add database

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,12 +1,8 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.host,
-  database: process.env.Database,
-  username: process.env.Username,
-  port: 3306,
-  password: process.env.Password,
-});
+const db = mysql.createConnection(
+  "mysql://f8u0cskxvdi841jj:q8zl21d4xuys8k3e@wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g0tvm76q649v2pp0"
+);
 
 db.connect(function (err) {
   if (err) throw err;
