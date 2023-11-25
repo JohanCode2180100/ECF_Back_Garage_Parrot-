e5a70fb2f2f161acbc746267f1552c24b1713e3b
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-25 23:12:42 +0100

    add postgres and init database URI

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,16 +1,10 @@
-const mysql = require("mysql2");
+const postgres = require("postgres");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const db = postgres(process.env.DATABASE_URL);
 
 db.connect(function (err) {
   if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
+  console.log("Connecté à la base de données !");
 });
 
 module.exports = db;
