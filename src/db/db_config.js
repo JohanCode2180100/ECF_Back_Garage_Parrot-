8a369da03832f3e3e7ec294cb5f5964661568017
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-11-25 23:30:43 +0100

    deleted request db connection

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -2,9 +2,4 @@ const postgres = require("postgres");
 
 const db = postgres(process.env.DATABASE_URL);
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connecté à la base de données !");
-});
-
 module.exports = db;
