df6f6a68dd8aef6f4b5069d270ad6c6d45a26fc1
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-18 11:53:16 +0100

    updated database config

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,15 +1,17 @@
 const mysql = require("mysql2");
 const config = require("./config.json");
 
+let db;
 
-const db = mysql.createConnection(
-  process.env.JAWSDB_URL || {
+if (process.env.NODE_ENV === "development") {
+  db = mysql.createConnection({
     host: config.host,
     user: config.user,
     database: config.database,
-  }
-);
-
+  });
+} else {
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+}
 
 db.connect((err) => {
   if (err) console.log(err.message);
