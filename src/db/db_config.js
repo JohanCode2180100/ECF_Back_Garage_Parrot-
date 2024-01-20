d7b379d55a73e42be0c38822d259ac0b3df54e0b
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-20 15:54:24 +0100

    updated connection config process.env'production' and 'environment'

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql2");
 const config = require("./config.json");
 
-const db = mysql.createConnection(
-  process.env.JAWSDB_URL || {
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = mysql.createConnection(
+    "mysql://pnaolnsapr1qksjt:la2jyst62spg9k5v@x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j0bddmuwnl0317em"
+  );
+} else {
+  db = mysql.createConnection({
     host: config.host,
     user: config.user,
     database: config.database,
-  }
-);
+  });
+}
 
 db.connect((err) => {
   if (err) console.log(err.message);
