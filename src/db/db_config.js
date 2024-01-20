a337bafcece6dbd8d90ac34e6c2cc15ad667a589
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-20 18:32:12 +0100

    updated config database

--- src/db/db_config.js
+++ src/db/db_config.js
@@ -4,9 +4,7 @@ const config = require("./config.json");
 let db;
 
 if (process.env.NODE_ENV === "production") {
-  db = mysql.createConnection(
-    "mysql://pnaolnsapr1qksjt:la2jyst62spg9k5v@x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j0bddmuwnl0317em"
-  );
+  db = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   db = mysql.createConnection({
     host: config.host,
